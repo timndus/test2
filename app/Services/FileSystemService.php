1aca6e941d506e7adc01184354a108f1c17d07ca
Ali Moradi Alamdarloo <timndus@gmail.com>
2021-11-11 10:13:41 +0000

    check file/directory exists

--- app/Services/FileSystemService.php
+++ app/Services/FileSystemService.php
@@ -2,20 +2,33 @@
 
 namespace App\Services;
 
-use App\Settings\Setting;
+use App\Settings\FileSystemSetting;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
 
 class FileSystemService implements \App\Interfaces\Services\IFileSystemService
 {
+    public function __construct(
+        private FileSystemSetting $setting
+    ) {}
+
     public function createDirectory(string $path): void {
+        if(File::isDirectory(__DIR__ . '/../../storage/app' . $path)) {
+            err($this->setting::HTTP_CODE_CONFLICT, $this->setting::DIRECTORY_EXISTS);
+        }
+
         if(!Storage::makeDirectory($path)) {
-            err(Setting::HTTP_CODE_INTERNAL_SERVER_ERROR, Setting::INTERNAL_SERVER_ERROR);
+            err($this->setting::HTTP_CODE_INTERNAL_SERVER_ERROR, $this->setting::INTERNAL_SERVER_ERROR);
         }
     }
 
     public function createFile(string $path): void {
+        if(File::isFile(__DIR__ . '/../../storage/app' . $path)) {
+            err($this->setting::HTTP_CODE_CONFLICT, $this->setting::FILE_EXISTS);
+        }
+
         if(!Storage::put($path, 'test')) {
-            err(Setting::HTTP_CODE_INTERNAL_SERVER_ERROR, Setting::INTERNAL_SERVER_ERROR);
+            err($this->setting::HTTP_CODE_INTERNAL_SERVER_ERROR, $this->setting::INTERNAL_SERVER_ERROR);
         }
     }
 
---
+++ app/Settings/FileSystemSetting.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Settings;
+
+class FileSystemSetting extends Setting {
+    public const FILE_EXISTS = 'file exists';
+    public const DIRECTORY_EXISTS = 'directory exists';
+}
