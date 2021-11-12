970e4f9fad0817492c58fd56e74df472a10f6a77
Ali Moradi Alamdarloo <timndus@gmail.com>
2021-11-12 08:49:20 +0000

    daily backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('horizon:snapshot')->everyFiveMinutes();
-        $schedule->command('job:start BackupAccountMedia')->everyTwoMinutes();
+        $schedule->command('job:start BackupAccountMedia')->daily();
     }
 
     /**
